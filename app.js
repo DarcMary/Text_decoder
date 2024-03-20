7e5a73d2fa665b179fb220ead51a917d290fb37b
Darc Mary <darcmary12@gmail.com>
2024-03-20 02:26:24 -0300

    Criptografia feita

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
-const chaves = {
-    "e": "enter",
-    "i": "imes",
-    "a": "ai",
-    "o": "ober",
-    "u": "ufat",
-};
+const encryptButton = document.querySelector(".botoes__criptografar");
+const inputTextarea = document.querySelector(".input_text_area");
+const outputTextarea = document.querySelector(".output_text_area");
+
+function encryptText(text) {
+    const chaves = {
+        "e": "enter",
+        "i": "imes",
+        "a": "ai",
+        "o": "ober",
+        "u": "ufat",
+    };
+
+    let encryptedText = "";
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i].toLowerCase();
+        const replacement = chaves[char];
+        encryptedText += replacement !== undefined ? replacement : text[i];
+    }
+
+    return encryptedText;
+}
+
+encryptButton.addEventListener("click", function() {
+    const inputText = inputTextarea.value;
+    const encryptedText = encryptText(inputText);
+    outputTextarea.value = encryptedText;
+});
+
+const copiarButton = document.querySelector(".botao_copiar");
+copiarButton.addEventListener("click", function() {
+    outputTextarea.select();
+    document.execCommand("copy");
+});

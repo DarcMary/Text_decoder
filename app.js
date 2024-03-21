f51f77f320e0b41aa1837ecc508fcf8ab5be7c9f
Darc Mary <darcmary12@gmail.com>
2024-03-21 00:15:30 -0300

    Criptografia feita

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const encryptButton = document.querySelector(".botoes__criptografar");
-const inputTextarea = document.querySelector(".input_text_area");
-const outputTextarea = document.querySelector(".output_text_area");
+const encryptButton = document.querySelector(".buttons__encrypt");
+const decryptButton = document.querySelector(".buttons__decrypt");
+const inputTextArea = document.querySelector(".input_text_area");
+const outputTextArea = document.querySelector(".output_text_area");
+const copiarButton = document.querySelector(".copy_button");
 
 function encryptText(text) {
     const chaves = {
+        "a": "ai",
         "e": "enter",
         "i": "imes",
-        "a": "ai",
         "o": "ober",
         "u": "ufat",
     };
@@ -23,13 +25,61 @@ function encryptText(text) {
 }
 
 encryptButton.addEventListener("click", function() {
-    const inputText = inputTextarea.value;
+    const inputText = inputTextArea.value;
     const encryptedText = encryptText(inputText);
-    outputTextarea.value = encryptedText;
+    outputTextArea.value = encryptedText;
+    inputTextArea.value = ""
 });
 
-const copiarButton = document.querySelector(".botao_copiar");
 copiarButton.addEventListener("click", function() {
-    outputTextarea.select();
+    outputTextArea.select();
     document.execCommand("copy");
+    outputTextArea.value = "";
 });
+
+ function decryptText(text) {
+    const chaves = {
+        "ai": "a",
+        "enter": "e",
+        "imes": "i",
+        "ober": "o",
+        "ufat": "u",
+    };
+
+    let decryptedText = "";
+    let substring = "";
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+
+        if (chaves[substring + char] !== undefined) {
+            substring += char;
+        } else {
+            if (substring !== "") {
+                decryptedText += chaves[substring];
+                substring = "";
+            }
+            decryptedText += char;
+        }
+    }
+
+    if (substring !== "") {
+        decryptedText += chaves[substring];
+    }
+
+    return decryptedText;
+}
+
+decryptButton.addEventListener("click", function() {
+    const inputText = inputTextArea.value;
+    const decryptedText = decryptText(inputText);
+    outputTextArea.value = decryptedText;
+}); 
+
+//"a": "ai",
+//"e": "enter",
+//"i": "imes",
+//"o": "ober",
+//"u": "ufat",
+const matrizCodigo = [["a" , "ai"], ["e" , "enter"], ["i" , "imes"], ["o" , "ober"], ["u" , "ufat"]];
+console.table(matrizCodigo);
